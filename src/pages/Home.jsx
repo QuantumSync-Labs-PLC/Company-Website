5d9c2662e449bb654378241d1902b8daf309efa3
Tharindu Danushka <labsquantumsync@gmail.com>
2025-07-06 21:30:17 +0530

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,6 @@ export default function Home() {
       <Header />
       <main className="flex-grow flex flex-col">
         <HeroSection />
-        <ServicesSection />
         <WhyUsSection />
         <ProjectsSection />
         <TechStackSection />
